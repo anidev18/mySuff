e4a87048d01352dc103639c477d3cc219b15938a
anidev18 <79595150+anidev18@users.noreply.github.com>
2021-05-24 19:17:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+var err_sm = document.getElementById("ace_constant_err_sm");
+var err_title = document.getElementById("ace_constant_err");
+var err_cur = document.getElementById("ace_constant_curse");
+
+alert("This is what I SUFFER everyday. send help.")
+
+function run() {
+    // alert("dtata")
+    var err_sm = document.getElementById("ace_constant_err_sm");
+    var err_title = document.getElementById("ace_constant_err");
+    var err_cur = document.getElementById("ace_constant_curse");
+
+    err_title.style.display = "none"
+    err_cur.style.display = "none"
+
+    err_sm.style.display = "block"
+    err_sm.innerHTML = ""
+
+    setTimeout(function () { err_sm.innerHTML += "E_WARNING | E_NOTICE”. Thept E_ERROR, E_WAARSE, and E_NOTICE<br>" }, 50)
+    setTimeout(function () { err_sm.innerHTML += "y-2021 17:46:43 UTCannot modify header information<br>" }, 100)
+    setTimeout(function () { err_sm.innerHTML += "quire_once('/home4...')#7 /home4/graphg<br>" }, 150)
+    setTimeout(function () { err_sm.innerHTML += ":46:6:43ome4/ Cannot modRNING | E_NOTICE”. Thept E_ERfy he<br>" }, 200)
+    setTimeout(function () { err_sm.innerHTML += "RNING | E_NOTICE”. Thept E_ER<br>" }, 250)
+    setTimeout(function () { err_sm.innerHTML += "E_WARRNING | E_NOTICE”. Thept adgadCE<br>" }, 300)
+    setTimeout(function () { err_sm.innerHTML += "E_WARNRNING | E_NOTICE”. Thept E_ER E_WAARSE, and E_NOTICE<br>" }, 350)
+    setTimeout(function () { err_sm.innerHTML += "RNIN17:46:43 UTC] PHP WarningThept EE_NOT17r:46:43 UTC] PHP WE<br>" }, 400)
+    setTimeout(function () { err_sm.innerHTML += "2021 17 | NING | E_NOTICE”. Thept E_UTC] PHP Warning E_ERICE<br>" }, 450)
+    setTimeout(function () { err_sm.innerHTML += "C] PHP Warning:  TICE”. Thept E_RNNING | E_NOTICE”. Thept E_| aningRSE, and E_NOTICE<br>" }, 500)
+
+
+    setTimeout(function () { err_sm.style.display = "none"; dispranderr() }, 500)
+
+}
+first_time = true
+function dispranderr() {
+    var err_sm = document.getElementById("ace_constant_err_sm");
+    var err_title = document.getElementById("ace_constant_err");
+    var err_cur = document.getElementById("ace_constant_curse");
+
+    err_title.innerHTML = 0
+
+    while (err_title.innerHTML < 999) {
+        err_title.innerHTML = (Math.random() * 10000 | 0)
+    }
+    err_title.innerHTML += " Errors found"
+
+    if (first_time) {
+        err_cur.innerHTML = "Chintu can code better than YOU"
+        first_time = false
+    } else {
+        switch (Math.random() * 5 | 0) {
+            case 1: err_cur.innerHTML = "dear dumb coder, YOU suck"
+                break
+            case 2: err_cur.innerHTML = "YOU are pp coder. chintu laughs"
+                break
+            case 3: err_cur.innerHTML = "go and programm in scratch, its hard for YOU"
+                break
+            case 4: err_cur.innerHTML = "You again? mannual driller laugh at YOU"
+                break
+            default: err_cur.innerHTML = "quit allready YOU NOOb PHP is easy"
+                break
+        }
+    }
+    err_cur.innerHTML += "<br> <spam style='font-size:20px'>press \"Run Code\" again"
+    err_title.style.display = "block"
+    err_cur.style.display = "block"
+}
+
+
